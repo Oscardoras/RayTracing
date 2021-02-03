ffe14ac97d3d337d8ec7edaca6d0e67013baa4cc
Oscar Buon Ryd <oscardoras@gmail.com>
2021-02-03 19:19:43 +0100

    Update.

--- src/materials/Dielectric.hpp
+++ src/materials/Dielectric.hpp
@@ -20,6 +20,7 @@ public:
 	virtual LightData color(World const& world, Point const& point, Vector const& reflected, Vector const& faceDirection, int const& remaningRays, int const& maxDepth) const override {
 		Color albe;
 		Color light;
+		long id = long(this);
 
 		bool out = reflected*faceDirection > 0.;
 		float n1;
@@ -34,30 +35,32 @@ public:
 		
 		float cos_theta = fmin(-1*reflected.unit()*faceDirection.unit(), 1.);
         float sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+		float R0_sqrt = (n1-n2)/(n1+n2);
+		float R0 = R0_sqrt*R0_sqrt;
+		float R = R0 + (1.-R0)*pow(1. - cos_theta, 5);
+
 		if ((n1/n2)*sin_theta <= 1.) {
 			Vector const& n = faceDirection;
 			Vector const& R1 = reflected;
 			Vector const& R_ort = (n1/n2)*( R1 -(R1*n)*n );
 			Vector const& R_para = -sqrt( 1 - R_ort.lengthSquared() )*n;
 			Vector const& R2 = R_ort + R_para;
-			/*
-			Vector const& t = cross(n, cross(n, V2));
-			float sin_theta_1 = (n2/n1) * sqrt(1 - pow( n*V2 / ( n.length() * V2.length() ) , 2) );
-			float cos_theta_1 = sqrt(1 - sin_theta_1*sin_theta_1);
-			Vector refract = cos_theta_1*(n.unit()) + sin_theta_1*(t.unit());*/
 
 			LightData lightData = world.trace(Ray(point, R2), std::max(1, int(remaningRays/2)), maxDepth-1);
-			albe += lightData.albedo;
-			light += lightData.light;
-		} else {
+			albe += (1-R)*lightData.albedo;
+			light += (1-R)*lightData.light;
+			id = (id + lightData.id)/2;
+		}
+		{
 			Vector reflect = (reflected - 2*(reflected*faceDirection)*faceDirection).unit();
 			LightData lightData = world.trace(Ray(point, reflect), std::max(1, int(remaningRays/2)), maxDepth-1);
 
-			albe += lightData.albedo;
-			light += lightData.light;
+			albe += R*lightData.albedo;
+			light += R*lightData.light;
+			id = (id + lightData.id)/2;
 		}
 
-		return LightData(albedo*albe, light, long(this));
+		return LightData(albedo*albe, light, id);
 	}
 
 };
